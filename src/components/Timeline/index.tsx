49fb0b52ef87a71a0e6b663c4fe48eb42bce524e
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-10-06 20:53:36 +0900

    refactor: remove unused code

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,9 +1,5 @@
-import {
-  IGridviewPanelProps,
-  SplitviewReact,
-  SplitviewReadyEvent,
-} from "dockview";
-import { useCallback, useRef } from "react";
+import { IGridviewPanelProps } from "dockview";
+import { useRef } from "react";
 
 import { TimelineController } from "./TimelineController";
 import { TrackLayers } from "./TrackLayers";
@@ -12,23 +8,6 @@ type Props = {} & IGridviewPanelProps<{}>;
 
 export const Timeline: React.FC<Props> = () => {
   const controller = useRef<TimelineController>(new TimelineController());
-  const onReady = useCallback((e: SplitviewReadyEvent) => {
-    const left = e.api.addPanel({
-      id: "controls",
-      component: "controls",
-      params: { title: "", controller: controller.current },
-      minimumSize: 500,
-    });
-
-    const right = e.api.addPanel({
-      id: "track_layers",
-      component: "track_layers",
-      params: { title: "", controller: controller.current },
-      minimumSize: 500,
-    });
-
-    left.api.setSize({ size: 500 });
-  }, []);
 
   return (
     <div className="h-full">
