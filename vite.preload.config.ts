e0fca1f66fb60a1bb2508a6eabfb9203633d2af0
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-10-07 21:32:01 +0900

    refactor: use alias

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,4 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: /@fontinalia-constants\//, replacement: "/src/constants/" },
+      { find: /@fontinalia-main\//, replacement: "/src/main/" },
+      { find: /@fontinalia-remotion\//, replacement: "/src/remotion/" },
+      { find: /@fontinalia-schema\//, replacement: "/src/schema/" },
+      { find: /@fontinalia-shared\//, replacement: "/src/shared/" },
+    ],
+  },
+});
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: /@fontinalia-constants\//, replacement: "/src/constants/" },
+      { find: /@fontinalia-renderer\//, replacement: "/src/renderer/" },
+      { find: /@fontinalia-remotion\//, replacement: "/src/remotion/" },
+      { find: /@fontinalia-schema\//, replacement: "/src/schema/" },
+      { find: /@fontinalia-shared\//, replacement: "/src/shared/" },
+    ],
+  },
 });
